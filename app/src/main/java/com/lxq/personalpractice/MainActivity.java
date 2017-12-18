91acba70064127545c577fb7a8b6b8cacc76d534
lxq <842202389@qq.com>
2017-12-18 23:36:09 +0800

    引入butterKnife

--- app/src/main/java/com/lxq/personalpractice/MainActivity.java
+++ app/src/main/java/com/lxq/personalpractice/MainActivity.java
@@ -1,14 +1,32 @@
 package com.lxq.personalpractice;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.lxq.personalpractice.base.BaseActivity;
 
+import butterknife.BindView;
+
+/**
+ * @author Created by lxq on 2017/12/18.
+ */
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tvTest)
+    TextView tvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected int initLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        tvTest.setText("dadsdsdsa");
     }
 }
--- app/src/main/java/com/lxq/personalpractice/base/BaseActivity.java
+++ app/src/main/java/com/lxq/personalpractice/base/BaseActivity.java
@@ -1,10 +1,32 @@
 package com.lxq.personalpractice.base;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import butterknife.ButterKnife;
 
 /**
- * Created by Administrator on 2017/12/18.
+ * @author Created by lxq on 2017/12/18.
+ * 基类
  */
+public abstract class BaseActivity extends Activity {
 
-public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(initLayoutId());
+        ButterKnife.bind(this);
+        initView();
+    }
+
+    /**
+     * 引入布局
+     */
+    protected abstract int initLayoutId();
+
+    /**
+     * 初始化布局
+     */
+    protected abstract void initView();
 }
