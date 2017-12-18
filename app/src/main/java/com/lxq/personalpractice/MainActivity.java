f4a472311e085baa17e25c6cff195dbbe6aa2718
lxq <842202389@qq.com>
2017-12-18 22:01:00 +0800

    首次上传工程

---
+++ app/src/main/java/com/lxq/personalpractice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lxq.personalpractice;
+
+import android.os.Bundle;
+
+import com.lxq.personalpractice.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/lxq/personalpractice/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.lxq.personalpractice.base;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2017/12/18.
+ */
+
+public class BaseActivity extends Activity {
+}
